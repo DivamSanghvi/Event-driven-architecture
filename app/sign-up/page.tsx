3e89b4310c0ae729a569517a42657ab5a1a28b88
Divam Sanghvi <divamsanghvi@gmail.com>
2025-08-12 00:39:20 +0530

    migrations

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSignUp } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+const Signup = () => {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
